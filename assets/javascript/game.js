e1252b74aad6c3ba0727f032850996bdf3985336
SpeedHunterSam <brian.s2@gmail.com>
2019-08-02 01:02:15 -0500

    fixed scope issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,15 @@
         let totalLosses = 0; //setting inital losses to zero
         let guessCountDown = 9; // user gets 9 tries before 1st loss
         let allGuessesString = ""; //empty string used to concatenate user inputs and display to viewport
+        
+      
 
-        //creating an object with an array and a random letter function (method)
-        const computerGuess = {
+     // get user guess from keyboard input
+
+document.onkeyup = function (event) {
+
+    //creating an object with an array and a random letter function (method)
+    const computerGuess = {
         alphabet : ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
 
         makeGuess: function() {
@@ -17,25 +23,11 @@
         },
 
       };
-      //creating another random guess function 
-      function anotherGuess(){            
-          var i = Math.floor(Math.random() * this.alphabet.length);
-            return this.alphabet[i];
-        };
 
-        // calling method to compare it later
-      storedGuess = computerGuess.makeGuess();
-    
-    
-      alert(storedGuess); //value check just to confirm that user can acually win
-    
-      storedGuess = storedGuess.toLowerCase(); //converting array to lowercase...could have just made it lowercase to begin with ;-)
-      
-
-     // get user guess from keyboard input
-
-    document.onkeyup = function (event) {
-        userGuess = event.key.toLowerCase();
+        storedGuess = computerGuess.makeGuess(); // calling method to compare it later
+        storedGuess = storedGuess.toLowerCase(); //converting array to lowercase...could have just made it lowercase to begin with ;-)
+        
+        userGuess = event.key.toLowerCase(); //converts user input to lowercase
 
         allGuessesString = allGuessesString.concat(userGuess + ", ") // concatenating string
         document.getElementById("allGuesses").innerHTML = "Your Guesses So Far: " + allGuessesString; //displaying complete string to html
@@ -43,7 +35,7 @@
         if(storedGuess === userGuess){
         score++;
         document.getElementById("wins").innerHTML = "Wins: " + score; //showing wins to html
-
+        
             
         }
         else{
@@ -55,12 +47,10 @@
                 totalLosses++;
                 guessCountDown = 9;
                 document.getElementById("losses").innerHTML = "Losses:  " + totalLosses; // showing losses to html
-               
+                
             }
         };
     };
-      
-
     /*
         Pseudo Code
 
